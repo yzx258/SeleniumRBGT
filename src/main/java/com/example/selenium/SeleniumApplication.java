3063c3034789b50fdb2fb2be371cbc04780cb878
yucw@itsmycar.cn <123456>
2020-09-15 17:43:00 +0800

    添加

--- src/main/java/com/example/selenium/SeleniumApplication.java
+++ src/main/java/com/example/selenium/SeleniumApplication.java
@@ -1,7 +1,5 @@
 package com.example.selenium;
 
-import com.example.selenium.task.TaskUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
