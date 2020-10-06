466e60eac46eb83a581b4b9b2b784bbfe4f3a192
yucw@itsmycar.cn <123456>
2020-10-06 15:00:45 +0800

    添加

--- src/main/java/com/example/selenium/bet/BetUtil.java
+++ src/main/java/com/example/selenium/bet/BetUtil.java
@@ -95,8 +95,15 @@ public class BetUtil {
             //点击确定按钮
             driver.findElement(By.xpath("//*[@id=\"login\"]/div[1]/div[3]/button")).click();
             Thread.sleep(1000);
+            try{
+                driver.findElement(By.xpath("//*[@id=\"indexann\"]/h2/div/i"));
+                Thread.sleep(1000);
+            }catch (Exception e){
+                System.out.println("获取不到，不报错");
+            }
+            Thread.sleep(1000);
             // 点击体育赛制界面 /html/body/div[5]/div[1]/div[2]/div/div/ul/li/a/div/div
-            driver.findElement(By.xpath("/html/body/div[5]/div[1]/div[2]/div/div/ul/li/a/div/div")).click();
+            driver.findElement(By.xpath("//*[@id=\"index\"]/div[2]/div/div/ul/li/a/div/div")).click();
             Thread.sleep(2000);
             // 点击彩票界面
             driver.findElement(By.xpath("//*[@id=\"header-wrap\"]/div[1]/div[2]/ul/li[9]/a")).click();
