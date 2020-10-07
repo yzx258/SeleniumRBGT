2b217058d78a13b885cc9ac80361c1232ac0b6f2
yucw@itsmycar.cn <123456>
2020-10-07 09:52:02 +0800

    添加

--- src/main/java/com/example/selenium/bet/BetUtil.java
+++ src/main/java/com/example/selenium/bet/BetUtil.java
@@ -335,8 +335,11 @@ public class BetUtil {
                 // log.info("期数 ：" + qs + ",购买：单");
             } else {
                 // 双
-                driver.switchTo().window(JB).findElement(By.xpath("/html/body/div[1]/div[2]/div[5]/div[2]/div[3]/div/div[" + div + "]/ul/li[3]/dl/dd[5]")).click();
-                fifoCache.put(sendBetKey, "2,4,6,8,0");
+                //driver.switchTo().window(JB).findElement(By.xpath("/html/body/div[1]/div[2]/div[5]/div[2]/div[3]/div/div[" + div + "]/ul/li[3]/dl/dd[5]")).click();
+                //fifoCache.put(sendBetKey, "2,4,6,8,0");
+                driver.switchTo().window(JB).findElement(By.xpath("/html/body/div[1]/div[2]/div[5]/div[2]/div[3]/div/div[" + div + "]/ul/li[3]/dl/dd[4]")).click();
+                fifoCache.put(sendBetKey, "1,3,5,7,9");
+
                 // log.info("期数 ：" + qs + ",购买：双");
             }
             log.info("==================");
