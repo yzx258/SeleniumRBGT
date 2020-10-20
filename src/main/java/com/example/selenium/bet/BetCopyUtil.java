b6a68b34b109105555d35aa2d42c341674099e2c
yucw@itsmycar.cn <123456>
2020-10-20 10:08:24 +0800

    BBI篮球下注V1.0 - 调试代码，设置下注初始值为10

--- src/main/java/com/example/selenium/bet/BetCopyUtil.java
+++ src/main/java/com/example/selenium/bet/BetCopyUtil.java
@@ -75,13 +75,9 @@ public class BetCopyUtil {
         SleepUtil.sleepUtil(2000);
         // 账号:dhxm2376
         WebElement zh = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/form/div[1]/div/div[1]/div/input"));
-        System.out.println(zh.getAttribute("placeholder"));
-        SleepUtil.sleepUtil(2000);
         zh.sendKeys("dhxm2376");
         // 密码:ycw15659512376"
         WebElement mm = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div/form/div[1]/div/div[2]/div[1]/input"));
-        System.out.println(mm.getAttribute("placeholder"));
-        SleepUtil.sleepUtil(2000);
         mm.sendKeys("ycw15659512376");
         SleepUtil.sleepUtil(2000);
         //点击确定按钮
