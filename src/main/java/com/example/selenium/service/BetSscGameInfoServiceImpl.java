cf0fc812e4533604ba26c5ca416ce9692ae1c04a
俞春旺 <yucw@itsmycar.cn>
2022-04-24 21:08:05 +0800

    自动化 - 调整代码

--- src/main/java/com/example/selenium/service/BetSscGameInfoServiceImpl.java
+++ src/main/java/com/example/selenium/service/BetSscGameInfoServiceImpl.java
@@ -35,9 +35,8 @@ public class BetSscGameInfoServiceImpl extends ServiceImpl<BetSscGameInfoMapper,
     @Override
     public BetSscGameInfo getBetSscGameInfo(String period, Integer sscNumType) {
         log.info("查询购买信息：期数：{}，类型：{}", period, sscNumType);
-        BetSscGameInfo betSscGameInfo =
-            this.baseMapper.selectOne(Wrappers.lambdaQuery(BetSscGameInfo.class).eq(BetSscGameInfo::getPeriod, period)
-                .eq(BetSscGameInfo::getSscNumType, sscNumType).eq(BetSscGameInfo::getKjHm, null));
+        BetSscGameInfo betSscGameInfo = this.baseMapper.selectOne(Wrappers.lambdaQuery(BetSscGameInfo.class)
+            .eq(BetSscGameInfo::getPeriod, period).eq(BetSscGameInfo::getSscNumType, sscNumType));
         log.info("查询购买信息：BetSscGameInfo：{}", JSONUtil.toJsonStr(betSscGameInfo));
         return betSscGameInfo;
     }
