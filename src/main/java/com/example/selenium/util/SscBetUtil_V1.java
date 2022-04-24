d904e6c3a081622cfce0a0ca0a21865787d7c3fa
俞春旺 <yucw@itsmycar.cn>
2022-04-24 10:50:30 +0800

    自动化 - 调整代码

--- src/main/java/com/example/selenium/util/SscBetUtil_V1.java
+++ src/main/java/com/example/selenium/util/SscBetUtil_V1.java
@@ -372,6 +372,7 @@ public class SscBetUtil_V1 {
         stringBuffer.append("\n比赛结果：黑单");
         // 发送 - 钉钉消息
         dingUtil.sendMassage(stringBuffer.toString());
+        System.out.println(stringBuffer.toString());
     }
 
     /***
